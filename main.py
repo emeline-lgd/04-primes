8c3ba82b93abf68bd980831d6ba0cefe02681283
emeline-lgd <122470795+emeline-lgd@users.noreply.github.com>
2024-11-07 11:04:38 +0000

    voici mon rendu 04-primes Emeline Lagarde Groupe 8

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
+"""
+Dans cet exercice un cherche a savoir si le nombre entier est premier
+avec les fonction  isprime(p) et main()
+"""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
-
-    # votre code ici
-
-    pass
-
+    """
+    Cette fonction nous permet de retourner un booléen 
+    si l'entier mis en argument "p" est premier(True) ou non (False)
+    """
+    premier = True
+    b= int(sqrt(p))
+    if p ==1 :
+        premier = False
+    elif p==2:
+        premier = True
+    else:
+        for i in range (2,b+1):
+            if p % i == 0 :
+                premier = False
+                break
+    return premier
 #### Fonction principale
 
 
 def main():
+    """
+    main() est la fonction principale qui print les nombre premier
+    """
 
     # vos appels à la fonction secondaire ici
 
